2f2911b5afa92955018774a934a69d5db75b4151
Anesu <ndoroansue01@gmail.com>
2022-02-15 14:03:34 +0200

    updated

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_restful import Api
+from routes import BookList, ReviewList, Book, Review
+
+BASE_URL = '/api'
 
 app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(BookList, f'{BASE_URL}/BookList/<book_id>')
+api.add_resource(Book, f'{BASE_URL}/Books/<book_id>')
+api.add_resource(ReviewList, f'{BASE_URL}/ReviewList')
+api.add_resource(Review, f'{BASE_URL}/Review')
+
 
 @app.route('/')
 def hello_world():
--- routes.py
+++ routes.py
@@ -3,3 +3,15 @@ from flask_restful import Resource
 class BookList(Resource):
     def get(self):
         return {'hello': 'from booklist'}
+    
+class Book(Resource):
+    def get(self, book_id):
+        return {'hello': 'from book {book_id}'}
+    
+class ReviewList(Resource):
+    def get(self):
+        return {'hello': 'from reviews'}
+    
+class Review(Resource):
+    def get(self):
+        return {'hello': 'from review'}
