835c116b5004b78155f6104ece084a36b6555da0
Anesu <ndoroansue01@gmail.com>
2022-02-22 13:53:31 +0200

    updated

--- routes.py
+++ routes.py
@@ -1,12 +1,21 @@
 from flask_restful import Resource
 from repository import Repository
 from flask import request
+from routes import BookList, ReviewList, Book, Review
+from unittest.mock import MagicMock
 
-repo = Repository()
+repository = Repository()
 
 class BookList(Resource):
+    def __init__(self, repo=repository):
+        self.repo = repo
+        
     def get(self):
         return [book.__dict__ for book in repo.books_get_all()]
+    
+    def post(self):
+        data = request.get_json()
+        return self.repo.book_add(data).__dict__
     
 class Book(Resource):
     def get(self, book_id):
